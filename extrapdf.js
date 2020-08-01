2eb47019a35a65a8df6f6afc2c9d23a5c10de4e9
enricorod <49712968+enricorod@users.noreply.github.com>
2020-08-01 17:30:45 +0200

    Update extrapdf.js

--- extrapdf.js
+++ extrapdf.js
@@ -1,22 +1,22 @@
-//selezionamento della palla
+//selezionamento del cursore
 const ball = document.querySelector("#ball")
 //posizione di partenza con la pagina avviata
 let mouseX = 0
 let mouseY = 0
-//stessa cosa ma con la palla
+//stessa cosa ma con il cursore
 let ballX = 0
 let ballY = 0
 //velocità di partenza, responsabile dell'effetto "palla che segue il cursore"
 let speed = 0.1
 
 function animate() {
-  //valore della distanza fra palla e mouse
+  //valore della distanza fra cursore e mouse
   let distX = mouseX - ballX
   let distY = mouseY - ballY
-  //la posizione della palla rispetto al mouse ha una leggera differenza causata da speed
+  //la posizione del cursore rispetto al mouse ha una leggera differenza causata da speed
   ballX = ballX + (distX * speed)
   ballY = ballY + (distY * speed)
-  //la posizione della palla è così determinata da palla stessa (perchè è dist a rappresentare il cursore)
+  //la posizione del cursore è così determinata da palla stessa (perchè è dist a rappresentare il cursore)
   ball.style.left = ballX + "px"
   ball.style.top = ballY + "px"
   //funzione in loop (animazione sempre costante)
